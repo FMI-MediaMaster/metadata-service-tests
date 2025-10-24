74a3e1f24690d43298643843e60089f873bc3b3d
Giulian617 <buzatugiulian@yahoo.com>
2025-10-24 19:57:15 +0300

    [UPDATE] Support /info and /recommendations

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ type TypeMap = {
     string: string;
     number: number;
     stringArray: string[];
+    object: Record<string, any>;
 };
 
 type Fields = Record<string, { type: keyof TypeMap }>;
@@ -13,7 +14,7 @@ type FromSchema<T extends Fields> = {
     [K in keyof T]: T[K]['type'] extends keyof TypeMap ? TypeMap[T[K]['type']] : unknown;
 };
 
-export const describeOption = <T extends Fields>(
+export const runTests = <T extends Fields>(
     server: any,
     endpoint: string,
     {
@@ -30,52 +31,68 @@ export const describeOption = <T extends Fields>(
         type: string;
     }
 ) => {
-    type Option = FromSchema<typeof fields>;
+    type Item = FromSchema<typeof fields>;
     const fieldKeys = Object.keys(fields);
+    const queryType = endpoint.split('/').pop();
+    const queryParameter = queryType === 'options' ? 'name' : 'id';
+    const checkFields = (item: Item) => {
+        expect(Object.keys(item)).toEqual(fieldKeys);
 
-    describe('GET /options', async () => {
-        endpoint = `${endpoint}/options`;
+        for (const [key, props] of Object.entries(fields)) {
+            expect(typeof item[key]).toBe(props['type']);
+            if (!('empty' in props && props['empty'] === true)) {
+                expect(item[key]).not.toBeNull();
+            }
+        }
+    };
 
-        validItems.forEach((name) => {
-            it(`Valid ${type} '${name}' gives results`, async () => {
-                const res = await request(server).get(`${endpoint}?name=${encodeURIComponent(name)}`);
-                expect(res.status).toBe(200);
-                expect(Array.isArray(res.body)).toBe(true);
-                expect(res.body.length).toBeGreaterThan(0);
+    describe(`GET /${queryType}`, async () => {
+        validItems.forEach((item) => {
+            it(`Query with valid ${type} '${item}' gives proper results`, async () => {
+                const { body, status } = await request(server).get(`${endpoint}?${queryParameter}=${encodeURIComponent(item)}`);
+                expect(status).toBe(200);
 
-                (res.body as Option[]).forEach((item) => {
-                    expect(Object.keys(item)).toEqual(fieldKeys);
-                    for (const [key, props] of Object.entries(fields)) {
-                        if (!('empty' in props && props['empty'] === true)) {
-                            expect(item[key]).not.toBeNull();
-                        }
-                    }
-                });
+                if (queryType === 'info') {
+                    expect(typeof body).toBe('object');
+                    expect(body).not.toBeNull();
+                    checkFields(body);
+                } else {
+                    expect(Array.isArray(body)).toBe(true);
+                    expect(body.length).toBeGreaterThan(0);
+                    (body as Item[]).forEach((item) => checkFields(item));
+                }
             });
         });
 
-        nonExistentItems.forEach((name) => {
-            it(`Non-existent ${type} '${name}' gives an empty array`, async () => {
-                const res = await request(server).get(`${endpoint}?name=${encodeURIComponent(name)}`);
-                expect(res.status).toBe(200);
-                expect(Array.isArray(res.body)).toBe(true);
-                expect(res.body.length).toBe(0);
+        nonExistentItems.forEach((item) => {
+            it(`Query with non-existent ${type} '${item}' gives empty result`, async () => {
+                const { body, status } = await request(server).get(`${endpoint}?${queryParameter}=${encodeURIComponent(item)}`);
+
+                if (queryType === 'info') {
+                    expect(status).toBe(404);
+                    expect(body).toHaveProperty('error');
+                    expect(body.error).toBe(`${type} not found`);
+                } else {
+                    expect(status).toBe(200);
+                    expect(Array.isArray(body)).toBe(true);
+                    expect(body.length).toBe(0);
+                }
             });
         });
 
         it('Error when no query', async () => {
-            const res = await request(server).get(`${endpoint}`);
-            expect(res.status).toBe(400);
-            expect(res.body).toHaveProperty('error');
-            expect(res.body.error).toBe('Missing parameter for the options method');
+            const { body, status } = await request(server).get(`${endpoint}`);
+            expect(status).toBe(400);
+            expect(body).toHaveProperty('error');
+            expect(body.error).toBe(`Missing parameter for the ${queryType} endpoint`);
         });
 
         invalidQueries.forEach((query) => {
             it(`Error with query '${query}'`, async () => {
-                const res = await request(server).get(`${endpoint}?${encodeURIComponent(query)}`);
-                expect(res.status).toBe(400);
-                expect(res.body).toHaveProperty('error');
-                expect(res.body.error).toBe('Missing parameter for the options method');
+                const { body, status } = await request(server).get(`${endpoint}?${encodeURIComponent(query)}`);
+                expect(status).toBe(400);
+                expect(body).toHaveProperty('error');
+                expect(body.error).toBe(`Missing parameter for the ${queryType} endpoint`);
             });
         });
     });
